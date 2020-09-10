da4adb6e79575dccf3449a1bb5b1af56826923fe
Datum <allen.james.west@gmail.com>
2020-09-10 18:32:53 +0000

    added index.html, working on form rather than text generation

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session 
 
 app = Flask(__name__)
+app.secret_key = 'hey lil momma lemme whithper in yer ear'
 messages = []
 
 
 def add_messages(username, message):
     """add messages to the messages list"""
-    messages.append('{}: {}'.format(username, message))
+    now = datetime.now().strftime('%H:%M:%S')
+    messages.append('({}) {}: {}'.format(now, username, message))
 
 
 def get_all_messages():
@@ -18,7 +21,7 @@ def get_all_messages():
 @app.route('/')
 def index():
     """ Main page with instructions """
-    return 'To send a message use /USERNAME/MESSAGE'
+    return render_template('templates/index.html')
 
 
 @app.route('/<username>')
